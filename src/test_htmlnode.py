3e7afb50d12bd7f73308260c889b075f9d8eddd1
ChadRM <chad.r.miller@gmail.com>
2025-12-13 09:59:42 -0600

    Chapter 2, Lesson 3

---
+++ src/test_htmlnode.py
@@ -0,0 +1,21 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_create1(self):
+        node = HTMLNode("a","anchor",props={"target":"_blank"})
+        self.assertEqual(node.props["target"],"_blank")
+    
+    def test_create2(self):
+        node = HTMLNode("html","hello world")
+        self.assertEqual(node.tag,"html")
+    
+    def test_props_to_html(self):
+        node = HTMLNode("a","anchor",props={"target":"_blank","href":"http://www.google.com"})
+        self.assertEqual(node.props_to_html(),' target="_blank" href="http://www.google.com" ')
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
