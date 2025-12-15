cf62ea88dcb3579750773825a37baf6d73c142ce
ChadRM <chad.r.miller@gmail.com>
2025-12-15 08:40:41 -0600

    Chapter 2, Lesson 6 complete

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,6 +17,13 @@ class TestTextNode(unittest.TestCase):
     def test_print(self):
         node = TextNode("This node has no URL",TextType.ITALIC)
         self.assertEqual(node.__repr__(),"TextNode(This node has no URL, italic)")
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -23,3 +25,19 @@ class TextNode():
         return f"TextNode({self.text}, {self.text_type.value})"
 
     
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type is TextType.TEXT:
+        return LeafNode(tag=None,value=text_node.text)
+    if text_node.text_type is TextType.BOLD:
+        return LeafNode(tag="b",value=text_node.text)
+    if text_node.text_type is TextType.ITALIC:
+        return LeafNode(tag="i",value=text_node.text)
+    if text_node.text_type is TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_node.text_type is TextNode.LINK:
+        return LeafNode(tag="a",value=text_node.text,props={"href":text_node.url})
+    if text_node.text_type is TextNode.IMAGE:
+        return LeafNode(tag="img",value="", props={"src":text_node.url,"alt":text_node.text})
+    else:
+        raise Exception("text_type unknown")
