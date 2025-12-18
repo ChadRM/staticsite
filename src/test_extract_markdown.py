0adbd9d3ee402207290c10866996fd4b66c11a6e
Chad R. Miller <chad.r.miller@gmail.com>
2025-12-18 19:08:26 +0000

    Chapter 3, Lesson 4 complete

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,13 @@
+import unittest
+from extract_markdown import *
+
+class MyTestCase(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+if __name__ == '__main__':
+    unittest.main()
