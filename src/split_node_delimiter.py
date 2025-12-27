2ade8de0a4ae33e04d43935e655b62cd4fa6882b
Chad R. Miller <chad.r.miller@gmail.com>
2025-12-27 17:32:15 +0000

    Chapter 3, Complete, with lots of cheating and crying.

--- src/split_node_delimiter.py
+++ src/split_node_delimiter.py
@@ -2,56 +2,94 @@ from textnode import TextNode,TextType
 from extract_markdown import extract_markdown_images, extract_markdown_links
 
 
-def split_nodes_delimiter(old_nodes,delimiter,text_type):
-    output = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            output.append(node)
-        else: 
-            new_nodes = node.text.split(delimiter)
-            this_node_list = []
-            if len(new_nodes) > 1:
-                this_node_list.append(TextNode(new_nodes[0],TextType.TEXT))
-                this_node_list.append(TextNode(new_nodes[1],text_type))
-                if len(new_nodes) > 2:
-                    this_node_list.append(TextNode(new_nodes[2],TextType.TEXT))
-            output.extend(this_node_list)
-    return output
+# def split_nodes_delimiter(old_nodes,delimiter,text_type):
+#     output = []
+#     for node in old_nodes:
+#         if node.text_type != TextType.TEXT:
+#             output.append(node)
+#         else:
+#             parts = node.text.split(delimiter)
+#             this_node_list = []
+#             for index in range(0,len(parts)):
+#                 if index % 2 == 0:
+#                     if len(parts[index]) > 0:
+#                         this_node_list.append(TextNode(parts[index],TextType.TEXT))
+#                 else:
+#                     this_node_list.append(TextNode(parts[index],text_type))
+#             output.extend(this_node_list)
+#     return output
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if text_type != TextType.TEXT:
+            num_delims = len(sections) - 1
+            if num_delims % 2 != 0:
+                raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
 def split_nodes_image(old_nodes):
-    output = []
-    for image in old_nodes:
-        if image.text_type != TextType.TEXT:
-            output.append(image)
-        else:
-            matches = extract_markdown_images(image.text)
-            match = matches[0]
-            splitter = f"![{match[0]}]({match[1]})"
-            print(splitter)
-            new_nodes = image.text.split(splitter,1)
-            print(new_nodes)
-            if new_nodes[0] != "":
-                output.append(TextNode(new_nodes[0],TextType.TEXT))
-            output.append(TextNode(match[0],TextType.IMAGE,match[1]))
-            if new_nodes[1] != "":
-                output.extend(split_nodes_image([TextNode(new_nodes[1],TextType.TEXT)]))
-    return output
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
 
 def split_nodes_link(old_nodes):
-    output = []
-    for link in old_nodes:
-        if link.text_type != TextType.TEXT:
-            output.append(link)
-        else:
-            matches = extract_markdown_links(link.text)
-            match = matches[0]
-            splitter = f"[{match[0]}]({match[1]})"
-            print(splitter)
-            new_nodes = link.text.split(splitter,1)
-            print(new_nodes)
-            if new_nodes[0] != "":
-                output.append(TextNode(new_nodes[0],TextType.TEXT))
-            output.append(TextNode(match[0],TextType.LINK,match[1]))
-            if new_nodes[1] != "":
-                output.extend(split_nodes_link([TextNode(new_nodes[1],TextType.TEXT)]))
-    return output
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
--- src/test_split_node_delimiter.py
+++ src/test_split_node_delimiter.py
@@ -12,7 +12,7 @@ class TestSplitNode(unittest.TestCase):
             self.assertEqual(node.text_type, TextType.TEXT)
 
     def test_bold(self):
-        node = TextNode("This is text with a **bold* word.*",TextType.TEXT)
+        node = TextNode("This is text with a **bold** word.",TextType.TEXT)
         new_nodes = split_nodes_delimiter([node],"**",TextType.BOLD)
         self.assertEqual(new_nodes[1].text_type,TextType.BOLD)
     
@@ -24,7 +24,7 @@ class TestSplitNode(unittest.TestCase):
     def test_starts(self):
         node = TextNode("**Starts** with bold this time.",TextType.TEXT)
         new_nodes = split_nodes_delimiter([node],"**",TextType.BOLD)
-        self.assertEqual(new_nodes[1].text_type,TextType.BOLD)
+        self.assertEqual(new_nodes[0].text_type,TextType.BOLD)
 
     def test_italics(self):
         node = TextNode("This is some _italics_ for you.",TextType.TEXT)
